19dff471b41069e140d75a12927455a2179823fc
Tony Wilson <agwilson@gmail.com>
2019-11-12 14:18:18 -0800

    Updates

---
+++ precache-manifest.3ab5b1cfca1028695db1ec4238f0c8c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6045d7670befe169658249a23262f8a5",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e92635f8ad6d3cdf9ee4",
+    "url": "/monsters-rolodex/static/css/main.cef4a45e.chunk.css"
+  },
+  {
+    "revision": "3f85d9169e1ecf70ea00",
+    "url": "/monsters-rolodex/static/js/2.1768bef8.chunk.js"
+  },
+  {
+    "revision": "e92635f8ad6d3cdf9ee4",
+    "url": "/monsters-rolodex/static/js/main.bfecd330.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
